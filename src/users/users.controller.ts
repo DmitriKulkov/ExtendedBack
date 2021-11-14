5ab97e8b5c52429d0fda1b3b56bc1ef9071ce344
DmitriKulkov <workmailDK1102@gmail.com>
2021-11-14 12:12:29 +0300

    restricting access by roles added
    restricting access by roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JWTAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -20,6 +22,8 @@ export class UsersController {
 
     @ApiOperation({summary: 'Get All Users'})
     @ApiResponse({status: 200, type: [User]})
+    @Roles("admin")
+    @UseGuards(RolesGuard)
     @UseGuards(JWTAuthGuard)
     @Get()
     getAll() {
