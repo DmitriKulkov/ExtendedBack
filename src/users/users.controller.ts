19e7b2778d9412f03968c22ad532e69710a516bd
DmitriKulkov <workmailDK1102@gmail.com>
2021-11-14 13:58:41 +0300

    Issuance of Roles Added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,8 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JWTAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Roles } from 'src/auth/roles-auth.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -28,5 +30,25 @@ export class UsersController {
     @Get()
     getAll() {
         return this.userService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Add Role'})
+    @ApiResponse({status: 200})
+    @Roles("admin")
+    @UseGuards(RolesGuard)
+    @UseGuards(JWTAuthGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.userService.addRole(dto);
+    }
+    
+    @ApiOperation({summary: 'Ban user'})
+    @ApiResponse({status: 200})
+    @Roles("admin")
+    @UseGuards(RolesGuard)
+    @UseGuards(JWTAuthGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.userService.ban(dto);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 
 @Injectable()
 export class UsersService {
-
     constructor(@InjectModel(User) private userRepository: typeof User,
                  private roleService: RolesService) {}
 
@@ -26,5 +27,27 @@ export class UsersService {
     async getUsersByEmail(email: string) {
         const user = await this.userRepository.findOne({where: {email}, include: {all: true}})
         return user;
+    }
+
+    async addRole(dto: AddRoleDto){
+        const user = await this.userRepository.findByPk(dto.userId);
+        const role = await this.roleService.getRoleByValue(dto.value);
+        if(user && role){
+            await user.$add('role', role.id);
+            return dto
+        }
+        throw new HttpException('User or Role not faund', HttpStatus.NOT_FOUND)
+    }
+
+    
+    async ban(dto: BanUserDto) {
+        const user = await this.userRepository.findByPk(dto.userId);
+        if(!user){
+            throw new HttpException('User not faund', HttpStatus.NOT_FOUND)
+        }
+        user.banned = true
+        user.banReason = dto.banReason
+        await user.save()
+        return user
     }
 }
