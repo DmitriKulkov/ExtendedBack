f52a8f76ec3dc008234e766dc68b9009ea1517f0
DmitriKulkov <workmailDK1102@gmail.com>
2021-11-14 14:34:16 +0300

    Validation Whis Pipes Added

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({example: 'exampe@exmail.com', description: "Email"})
+    @IsString({message: 'Must be String'})
+    @IsEmail({}, {message: 'Wrong Email'})
     readonly email: string;
     @ApiProperty({example: 'qwerty123', description: "Password"})
+    @IsString({message: 'Must be String'})
+    @Length(4, 16, {message: 'Must be less than 16 and greater than 4 symbols'})
     readonly password: string;
 }
